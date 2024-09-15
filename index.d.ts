986b32aad0eade1cfcaa39fcae2416052a77c8b6
Corey Olson <43089449+coreyolson@users.noreply.github.com>
2024-09-14 21:28:47 -0500

    Package cleanup

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare module 'financialmodelingprep' {
+    export default class FinancialModelingPrepClient {
+        constructor(apiKey: string, userOptions?: { timeout?: number, base?: string });
+        fetch(endpoint: string): Promise<any>;
+        any(version: string, endpoint: string, params?: object): Promise<any>;
+        getAllEndpoints(): Array<string>;
+        [key: string]: ((...params: any[]) => Promise<any>) | (() => Array<string>);
+    }
+}
